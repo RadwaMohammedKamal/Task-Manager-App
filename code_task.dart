2a9a64f8effcbf2379c610aa4b6f75cd5effd28e
RadwaMhammed <127146039+RadwaMhammed@users.noreply.github.com>
2025-10-27 20:19:03 +0300

    Update code_task.dart

---
+++ code_task.dart
@@ -0,0 +1,9 @@
+void main() {
+  print(' Hello world ,confelect task');
+  
+  addTask('Complete Git practice task');
+}
+
+void addTask(String taskName) {
+  print(' Task Added Successfully: $taskName');
+}
