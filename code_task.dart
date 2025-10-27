2a9d71f3dcbea51b83057ab4f4702cbac016a9f9
RadwaMhammed <127146039+RadwaMhammed@users.noreply.github.com>
2025-10-27 20:44:25 +0300

    Resolve conflict in code_task.dart

--- code_task.dart
+++ code_task.dart
@@ -1,5 +1,9 @@
 void main() {
+<<<<<<< HEAD
   print("Hello world, my local version")
+=======
+  print("Hello world, collaborator version")
+>>>>>>> 6ad7721 (colaborative code_task.dart)
   
   addTask('Complete Git practice task');
 }
